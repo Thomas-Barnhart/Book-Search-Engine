581726c96e6ebffb54e24f96202ede907e4ad8a2
Neal Chris <tmbarnhart1@gmail.com>
2023-12-21 16:08:41 -0500

    Comments for easier future reference

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,7 @@
+// Importing the gql function from the "@apollo/client" library
 import { gql } from "@apollo/client";
 
+// GraphQL mutation for user login
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -12,6 +14,7 @@ export const LOGIN_USER = gql`
   }
 `;
 
+// GraphQL mutation for adding a new user
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -24,6 +27,7 @@ export const ADD_USER = gql`
   }
 `;
 
+// GraphQL mutation for saving a book to a user's collection
 export const SAVE_BOOK = gql`
   mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
@@ -42,6 +46,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
+// GraphQL mutation for removing a book from a user's collection
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,7 @@
+// Importing the gql function from the "@apollo/client" library
 import { gql } from '@apollo/client';
 
+// GraphQL query for retrieving user information
 export const GET_ME = gql`
   {
     me {
