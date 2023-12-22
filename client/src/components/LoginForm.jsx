3a4f00a8d762ffa208f688589960902ba7ac1647
Neal Chris <tmbarnhart1@gmail.com>
2023-12-21 19:26:14 -0500

    Commented out the old utils create/login user

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
