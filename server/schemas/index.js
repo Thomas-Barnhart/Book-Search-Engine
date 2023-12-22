ac2c06455b83b247302ffa7368de86edd83359ed
Neal Chris <tmbarnhart1@gmail.com>
2023-12-21 19:00:11 -0500

    Index schema for typeDefs and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,8 @@
+// Importing type definitions from the './typeDefs' file
+const typeDefs = require('./typeDefs');
+
+// Importing resolvers from the './resolvers' file
+const resolvers = require('./resolvers');
+
+// Exporting the type definitions and resolvers as an object
+module.exports = { typeDefs, resolvers };
